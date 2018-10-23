2b36ac5e27c84d886521fa2ac38ff7ff69900efc
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-23 14:21:59 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,6 @@
 module Findable
-      def find_by_name(name)
-        ALL.detect{|a| a.name == name}
-      end
+  def find_by_name(name)
+    binding.pry
+    ALL.detect{|a| a.name == name}
+  end
 end
